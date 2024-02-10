0e5065680026d374846b7dcb766140f4e8b5060f
QQmental <39618121+QQmental@users.noreply.github.com>
2024-02-11 02:05:49 +0800

    bug fix

--- deferred_array.hpp
+++ deferred_array.hpp
@@ -139,14 +139,17 @@ private:
         decltype(auto) operator[](std::size_t idx) const noexcept
         {
             //assert(is_out_of_rangge(idx) == false);
-            auto next_stride_length = m_stride_length/m_boundary_arr[dimension-cnt];
-
-            Deferred_array<element_t, dimension>::Ptr_impl<cnt - 1> ret{m_ptr + idx*m_stride_length, m_boundary_arr, next_stride_length};
-
+            
             if constexpr (cnt > 1)
+            {
+                auto next_stride_length = m_stride_length/m_boundary_arr[dimension-cnt+1];
+
+                Deferred_array<element_t, dimension>::Ptr_impl<cnt - 1> ret{m_ptr + idx*m_stride_length, m_boundary_arr, next_stride_length};
+            
                 return ret;
+            }
             else
-                return *ret.m_ptr;
+                return m_ptr[idx];
         }
         friend Deferred_array<element_t, dimension>::Ptr_impl<cnt+1>;
     };
