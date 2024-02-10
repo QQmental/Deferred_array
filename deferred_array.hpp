50e9157e91cb93ff7b3b40afa8e7d0da28fa1c66
QQmental <39618121+QQmental@users.noreply.github.com>
2024-02-10 19:10:48 +0800

    Update deferred_array.hpp
    you can uncomment 'assert(is_out_of_rangge(idx) == false);' to enable array boundary check

--- deferred_array.hpp
+++ deferred_array.hpp
@@ -8,15 +8,13 @@ class Deferred_array
 {
 public:
     
+    inline static constexpr std::size_t dimension_length = dimension; 
     std::size_t data_len() const 
     {
-        std::size_t len = 1;
-        for(auto &&bound : m_dimension_boundary)
-            len *= bound;
-        return len;
+        return m_data_len;
     }
 
-    Deferred_array() : m_data(nullptr){}
+    Deferred_array() : m_data(nullptr), m_data_len(0){}
 
     Deferred_array(const std::initializer_list<std::size_t> &list) 
     {
@@ -24,12 +22,17 @@ public:
         
         std::copy(std::begin(list), std::end(list), std::begin(m_dimension_boundary));
 
+        m_data_len = 1;
+        for(auto &&bound : m_dimension_boundary)
+            m_data_len *= bound;
+        
         m_data = new element_t[data_len()];
     }
 
     Deferred_array(const Deferred_array<element_t, dimension> &src)
     {
         std::copy(std::begin(src.m_dimension_boundary), std::end(src.m_dimension_boundary), std::begin(m_dimension_boundary));
+        m_data_len = src.data_len();
 
         auto new_alloc = new char[sizeof(element_t) * data_len()];
         m_data = new(new_alloc)element_t;
@@ -40,17 +43,17 @@ public:
             auto src_ptr = new (src.m_data)element_t;
             dst_ptr[i] = src_ptr[i];
         }
-
-        *this = src;
     }
 
     Deferred_array(Deferred_array<element_t, dimension> &&src)
     {
         std::copy(std::begin(src.m_dimension_boundary), std::end(src.m_dimension_boundary), std::begin(m_dimension_boundary));
-        
+
         m_data = src.m_data;
+        m_data_len = src.m_data_len;
 
         src.m_data = nullptr;
+        src.m_data_len = 0;
     }
     ~Deferred_array()
     {
@@ -64,7 +67,8 @@ public:
         this->~Deferred_array();
         
         std::copy(std::begin(src.m_dimension_boundary), std::end(src.m_dimension_boundary), std::begin(m_dimension_boundary));
-    
+        m_data_len = src.m_data_len;
+
         auto new_alloc = new char[sizeof(element_t) * data_len()];
         m_data = new(new_alloc)element_t;
 
@@ -87,7 +91,10 @@ public:
         std::copy(std::begin(src.m_dimension_boundary), std::end(src.m_dimension_boundary), std::begin(m_dimension_boundary));
     
         m_data = src.m_data;
+        m_data_len = src.m_data_len;
+
         src.m_data = nullptr;
+        src.m_data_len = 0 ;
 
         return *this;
     }
@@ -98,20 +105,25 @@ public:
     
     auto operator[](int idx) const noexcept 
     {
-        auto ret = Deferred_array<element_t, dimension>::bracket_impl<dimension>{m_data, m_dimension_boundary}[idx];
+        auto ptr_stride_length = data_len()/m_dimension_boundary[0];
+        auto ret = Deferred_array<element_t, dimension>::Ptr_impl<dimension>{m_data, m_dimension_boundary, ptr_stride_length}[idx];
         return ret;
     }
 
 private:
     template<std::size_t cnt>
-    class bracket_impl
+    class Ptr_impl
     {
     private:
         element_t *m_ptr = nullptr;
         const std::size_t (&m_boundary_arr)[dimension];
+        std::size_t m_stride_length;
 
     public:
-        bracket_impl(element_t *ptr, const std::size_t (&boundary_arr)[dimension]) : m_ptr(ptr), m_boundary_arr(boundary_arr){};
+        Ptr_impl(element_t *ptr, const std::size_t (&boundary_arr)[dimension], std::size_t stride_length) 
+        : m_ptr(ptr), 
+          m_boundary_arr(boundary_arr),
+          m_stride_length(stride_length){};
 
         const element_t& operator*() const noexcept {return *m_ptr;}
         element_t& operator*() noexcept {return *m_ptr;}
@@ -126,21 +138,20 @@ private:
 
         decltype(auto) operator[](std::size_t idx) const noexcept
         {
-            assert(is_out_of_rangge(idx) == false);
+            //assert(is_out_of_rangge(idx) == false);
 
-            std::size_t dim_size = 1;
-            for(std::size_t i = dimension - cnt + 1 ; i < dimension ; i++)
-                dim_size *= m_boundary_arr[i];
+            auto next_stride_length = m_stride_length/m_boundary_arr[dimension-cnt+1];
 
-            Deferred_array<element_t, dimension>::bracket_impl<cnt - 1> ret{m_ptr + idx*dim_size, m_boundary_arr};
+            Deferred_array<element_t, dimension>::Ptr_impl<cnt - 1> ret{m_ptr + idx*m_stride_length, m_boundary_arr, next_stride_length};
 
             if constexpr (cnt > 1)
                 return ret;
             else
                 return *ret.m_ptr;
         }
-        friend class Deferred_array<element_t, dimension>::bracket_impl<cnt+1>;
+        friend Deferred_array<element_t, dimension>::Ptr_impl<cnt+1>;
     };
     element_t *m_data;
     std::size_t m_dimension_boundary[dimension] = {0};
+    std::size_t m_data_len;
 };
